aad06db83326b26a8f4c1ecd980eeb212aef6bed
Anthony Magpantay <anthony040190@yahoo.com>
2023-09-26 14:36:54 -0600

    Add another style on the main page

--- app/page.js
+++ app/page.js
@@ -3,10 +3,10 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className='container-fluid ml-40 mt-20'>
+    <div className='mt-20'>
       <p className='text-4xl text-center'>CPRG 306: Web Development 2 - Assignments</p>
       <div className='text-center mt-20'>
-        <p><StudentInfo /></p>
+        <StudentInfo />
         <p className='py-1'><Link href="/week2/">Week 2</Link></p>
         <p className='py-1'><Link href="/week3/">Week 3</Link></p>
       </div>
--- app/student-info.js
+++ app/student-info.js
@@ -3,15 +3,48 @@ import Link from "next/link";
 export default function StudentInfo() {
     return (
         <div>
-            <p className="py-1">Name: Anthony Magpantay</p>
+            <div className="flex py-1">
+                <div className="w-1/2 text-right pr-3">
+                    <label>Name:</label> 
+                </div>
+                
+                <div className="w-1/2 text-left pl-3">
+                    <p>Anthony Magpantay</p>
+                </div>
+            </div>
 
-            <p className="py-1">Course and section: CPRG 306 F</p>
+            {/* <div class="md:flex md:items-center mb-6">
+                <div class="w-1/2">
+                    <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
+                        Full Name
+                    </label>
+                </div>
+                <div class="w-1/2">
+                    <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" value="Jane Doe" />
+                </div>
+            </div> */}
 
-            <p className="py-1">
-                <Link href="https://github.com/anthony040190" className="underline" target="_blank">
-                    https://github.com/anthony040190
-                </Link>    
-            </p>
+            <div className="flex py-1">
+                <div className="w-1/2 text-right pr-3">
+                    <label>Course and section:</label>
+                </div>
+
+                <div className="w-1/2 text-left pl-3">
+                    <p>CPRG 306 F</p>
+                </div>
+            </div>
+
+            <div className="flex py-1">
+                <div className="w-1/2 text-right pr-3">
+                    <label>Github Account:</label>
+                </div>
+
+                <div className="w-1/2 text-left pl-3">
+                    <Link href="https://github.com/anthony040190" className="underline" target="_blank">
+                        https://github.com/anthony040190
+                    </Link>
+                </div>
+            </div>
         </div>
     );
 }
