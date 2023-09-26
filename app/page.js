6fef9e78f8b8b43dcf140524fd9f91956b732359
Anthony Magpantay <anthony040190@yahoo.com>
2023-09-26 13:51:08 -0600

    Put some styling

--- app/page.js
+++ app/page.js
@@ -4,17 +4,12 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <div className='container-fluid ml-40 mt-20'>
-      <p className='h1'>CPRG 306: Web Development 2 - Assignments</p>
-      <StudentInfo />
-      <Link href="/week2/">
-        Week 2
-      </Link>
-
-      <br />
-
-      <Link href="/week3/">
-        Week 3
-      </Link>
+      <p className='text-4xl text-center'>CPRG 306: Web Development 2 - Assignments</p>
+      <div className='text-center mt-20'>
+        <p><StudentInfo /></p>
+        <p className='py-1'><Link href="/week2/">Week 2</Link></p>
+        <p className='py-1'><Link href="/week3/">Week 3</Link></p>
+      </div>
     </div>    
   );
 }
--- app/student-info.js
+++ app/student-info.js
@@ -3,21 +3,15 @@ import Link from "next/link";
 export default function StudentInfo() {
     return (
         <div>
-            <div>
-                <p>Name: Anthony Magpantay</p>
-            </div>
+            <p className="py-1">Name: Anthony Magpantay</p>
 
-            <div>
-                <p>Course and section: CPRG 306 F</p>
-            </div>
+            <p className="py-1">Course and section: CPRG 306 F</p>
 
-            <div>
-                <p>
-                    <Link href="https://github.com/anthony040190">
-                        https://github.com/anthony040190
-                    </Link>    
-                </p>
-            </div>
+            <p className="py-1">
+                <Link href="https://github.com/anthony040190" className="underline" target="_blank">
+                    https://github.com/anthony040190
+                </Link>    
+            </p>
         </div>
     );
 }
